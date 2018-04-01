7953bf5e6d73ff7965a3600b98f13b0dc022f4e9
Caleb Leung <dvcs@calebleung.com>
2018-03-31 20:00:38 -0500

    Add cache. Cleaner return for /search

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 # SELECT * FROM vods INNER JOIN played ON played.vod_id = vods.vod_id INNER JOIN games ON played.game_id = games.rowid WHERE games.name='Board Games' ORDER BY vods.created_at DESC
 
 from flask import Flask, request, render_template
+from flask_caching import Cache
 from flask_restful import Resource, Api
 
 import configparser
@@ -10,11 +11,13 @@ app = Flask(__name__,
         static_folder = "./www/static",
         template_folder = "./www")
 api = Api(app)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 config = configparser.ConfigParser()
 config.read('config')
 
 class GamesList(Resource):
+    @cache.memoize(50)
     def get(self):
         games = []
         with sqlite3.connect(config['DB']['name']) as conn:
@@ -24,8 +27,9 @@ class GamesList(Resource):
         return {'games': games}
 
 class Search(Resource):
-    def get(self, searchString):
-        search = ('%{}%'.format(searchString),)
+    @cache.memoize(50)
+    def post(self):
+        search = ('%{}%'.format(request.form['data']),)
         games = []
         vods = []
         with sqlite3.connect(config['DB']['name']) as conn:
@@ -33,13 +37,13 @@ class Search(Resource):
             c.execute('SELECT rowid, name FROM games WHERE name LIKE ? ORDER BY name COLLATE NOCASE ASC', search)
             games = c.fetchall()
 
-            for game in games:
-                for row in c.execute('SELECT played.vod_id, played.start_at, vods.title, vods.animated_preview_url FROM played INNER JOIN vods ON vods.vod_id = played.vod_id WHERE game_id=:gameID', {'gameID': game[0]}):
-                    vods.append(row)
+            for i, game in enumerate(games):
+                for row in c.execute('SELECT :gameIndex, played.vod_id, played.start_at, vods.title, vods.created_at FROM played INNER JOIN vods ON vods.vod_id = played.vod_id WHERE game_id=:gameID', {'gameIndex': str(i), 'gameID': game[0]}):
+                    vods.append({'game_index': row[0], 'vod_id': row[1], 'start_at': row[2], 'date': row[4]})
         return {'games': games, 'vods': vods}
 
 api.add_resource(GamesList, '/games')
-api.add_resource(Search, '/search/<string:searchString>')
+api.add_resource(Search, '/search')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
