ec46e36a3e5b0e8b41499ae84ebdeaff5020de1c
Caleb Leung <dvcs@calebleung.com>
2018-04-18 10:30:22 -0500

    swap left/right swipe

--- src/www/static/mobile.js
+++ src/www/static/mobile.js
@@ -11,13 +11,13 @@ document.addEventListener('touchend', function (e) {
     var offset = 100;
     if (touchVal) {
         var touchEnd = e.changedTouches.item(0).clientX;
-        if (touchEnd < touchVal - offset) {
+        if (touchEnd > touchVal + offset) {
             if ($('#resultsNav').pagination('getSelectedPageNum') == 1) {
                 $('#resultsNav').pagination('go', $('#resultsNav').pagination('getTotalPage'));
             } else {
                 $('#resultsNav').pagination('previous');
             }
-        } else if (touchEnd > touchVal + offset) {
+        } else if (touchEnd < touchVal - offset) {
             if ($('#resultsNav').pagination('getSelectedPageNum') == $('#resultsNav').pagination('getTotalPage')) {
                 $('#resultsNav').pagination('go', 1);
             } else {
